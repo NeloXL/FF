92b6eaa8200545dab406e401020f1d54da393b78
Lem4n1d <leonid.baklanov2015@gmail.com>
2025-10-13 20:40:27 +0300

    Created the base of backend

--- friendfolio/routes/web.php
+++ friendfolio/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
+//Главная страница
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+// POST роуты для обработки
+Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Страница заметок
+Route::get('/notes', function () {
+    return view('notes');
+})->name('notes');

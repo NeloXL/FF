be0e1850ba7666b674b622ca458643465d0e4aa0
Lem4n1d <leonid.baklanov2015@gmail.com>
2025-10-27 13:15:33 +0300

    Сделал чуть поудобнее для себя header.blade.php и добавил шапку в notes.blade.php, так же сделал временную кнопку logout, взяв готовые стили

--- friendfolio/resources/views/components/header.blade.php
+++ friendfolio/resources/views/components/header.blade.php
@@ -9,6 +9,7 @@
 </head>
 <body>
 <header>
+    <!--**
     @guest
         <section class="logo">
             <img src="{{ asset('assets/img/logoFF.png') }}" style="width: 48px; aspect-ratio: 1/1" alt="Логотип" class="logo-img">
@@ -20,6 +21,25 @@
             <button class="button-black" id="openModal2">Зарегистрироваться</button>
         </section>
     @endguest
+
+        **-->
+    <section class="logo">
+        <img src="{{ asset('assets/img/logoFF.png') }}" style="width: 48px; aspect-ratio: 1/1" alt="Логотип" class="logo-img">
+        <div class="logo-text regular">FriendFolio</div>
+    </section>
+    @guest
+        <section class="buttons">
+            <button class="button-white"  id="openModal">Войти</button>
+            <button class="button-black" id="openModal2">Зарегистрироваться</button>
+        </section>
+    @endguest
+    @auth
+        <section class="buttons">
+            <form action="{{route("logout")}}">
+                <button class="button-black">Выйти</button>
+            </form>
+        </section>
+    @endauth
 </header>
 
 <dialog id="modal" class="modal">
